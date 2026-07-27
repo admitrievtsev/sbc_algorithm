a1e4d799e3294ccc82804f1620cbd9cf66f20ece
Denis Potapov <DenisPotapov-0@yandex.ru>
2026-07-27 22:33:12 +0300

    fix merge

--- marline_palantir/src/types/mod.rs
+++ marline_palantir/src/types/mod.rs
@@ -11,7 +11,9 @@
 
 mod block_id;
 mod chunk;
+mod chunk_hash;
 mod super_feature;
 pub use block_id::BlockID;
 pub use chunk::Chunk;
+pub use chunk_hash::ChunkDigest;
 pub use super_feature::{SuperFeature, SuperFeatureGenerator, TierConfig};
