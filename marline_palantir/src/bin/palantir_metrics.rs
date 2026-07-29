85b0ee0765583e8e5132b78519771c928f27313f
Makar Lill <fjfjhfhf6@gmail.com>
2026-07-29 16:07:40 +0300

    benchmark(palantir)

--- marline_palantir/src/bin/palantir_metrics.rs
+++ marline_palantir/src/bin/palantir_metrics.rs
@@ -4,7 +4,7 @@ use std::time::Instant;
 
 use chunkfs::chunkers::{FastChunker, SizeParams};
 use chunkfs::hashers::Sha256Hasher;
-use chunkfs::{Chunker, DataContainer, Hasher, Scrub};
+use chunkfs::{DataContainer, FileSystem};
 use marline_palantir::encoder::GdeltaEncoder;
 use marline_palantir::lifecycle_manager::LifecycleManager;
 use marline_palantir::mock_rocksdb::MockRocksDBMap;
@@ -45,165 +45,53 @@ fn collect_files(dir: &Path, files: &mut Vec<Vec<u8>>) {
     }
 }
 
-fn chunk_file(data: &[u8], chunk_size: SizeParams) -> (HashMap<[u8; 32], DataContainer<[u8; 32]>>, u64) {
-    let mut chunker = FastChunker::new(chunk_size);
-    let chunks = chunker.chunk_data(data, Vec::new());
-    let mut hasher = Sha256Hasher::default();
-    let mut db = HashMap::with_capacity(chunks.len());
-    let mut total = 0u64;
-    for c in &chunks {
-        let chunk_data = data[c.offset()..c.offset() + c.length()].to_vec();
-        let hash = hasher.hash(&chunk_data);
-        total += chunk_data.len() as u64;
-        db.insert(hash, DataContainer::from(chunk_data));
-    }
-    (db, total)
-}
-
 fn run_metrics(
     name: &str,
-    tier_list: &[u32],
-    features_num_override: Option<usize>,
+    scrubber: impl chunkfs::Scrub<[u8; 32], HashMap<[u8; 32], DataContainer<[u8; 32]>>, [u8; 32], MockRocksDBMap> + 'static,
     kernel_files: &[Vec<Vec<u8>>],
 ) {
-    let chunk_size = SizeParams::new(16 * 1024, 32 * 1024, 64 * 1024);
-    let mut target = MockRocksDBMap::new();
-    let mut original_total = 0u64;
+    let database: HashMap<[u8; 32], DataContainer<[u8; 32]>> = HashMap::default();
+    let target_map = MockRocksDBMap::new();
+    let hasher = Sha256Hasher::default();
 
+    let mut fs = FileSystem::new_with_scrubber(
+        database,
+        target_map,
+        Box::new(scrubber),
+        hasher,
+    );
+
+    let chunk_size = SizeParams::new(8192, 32768, 65536);
+    let original_total: usize = kernel_files.iter().flat_map(|f| f.iter()).map(|d| d.len()).sum();
     let start = Instant::now();
-    let (delta_stored, fp_size, sf_size) = match tier_list.len() {
-        1 => {
-            let arr: [u32; 1] = tier_list.try_into().unwrap();
-            let tc = if let Some(fn_val) = features_num_override {
-                TierConfig::with_features_num(arr, fn_val)
-            } else {
-                TierConfig::new(arr)
-            };
-            let sf_gen = if let Some(fn_val) = features_num_override {
-                PalantirHasher::with_features_num(7, tier_list.to_vec(), fn_val)
-            } else {
-                PalantirHasher::new(7, tier_list.to_vec())
-            };
-            let mut scrubber = PalantirScrubber::new(
-                sf_gen, GdeltaEncoder, tc,
-                LifecycleManager::<1>::default_configs(),
-            );
-            for files in kernel_files {
-                for data in files {
-                    let (mut db, file_total) = chunk_file(data, chunk_size);
-                    if db.is_empty() { continue; }
-                    original_total += file_total;
-                    scrubber.scrub(&mut db, &mut target).unwrap();
-                }
-                scrubber.update().unwrap();
-            }
-            (scrubber.delta_stored(), scrubber.fp_table_size(), scrubber.sf_table_size())
-        }
-        2 => {
-            let arr: [u32; 2] = tier_list.try_into().unwrap();
-            let tc = TierConfig::new(arr);
-            let sf_gen = PalantirHasher::new(7, tier_list.to_vec());
-            let mut scrubber = PalantirScrubber::new(
-                sf_gen, GdeltaEncoder, tc,
-                LifecycleManager::<2>::default_configs(),
-            );
-            for files in kernel_files {
-                for data in files {
-                    let (mut db, file_total) = chunk_file(data, chunk_size);
-                    if db.is_empty() { continue; }
-                    original_total += file_total;
-                    scrubber.scrub(&mut db, &mut target).unwrap();
-                }
-                scrubber.update().unwrap();
-            }
-            (scrubber.delta_stored(), scrubber.fp_table_size(), scrubber.sf_table_size())
-        }
-        3 => {
-            let arr: [u32; 3] = tier_list.try_into().unwrap();
-            let tc = TierConfig::new(arr);
-            let sf_gen = PalantirHasher::new(7, tier_list.to_vec());
-            let mut scrubber = PalantirScrubber::new(
-                sf_gen, GdeltaEncoder, tc,
-                LifecycleManager::<3>::default_configs(),
-            );
-            for files in kernel_files {
-                for data in files {
-                    let (mut db, file_total) = chunk_file(data, chunk_size);
-                    if db.is_empty() { continue; }
-                    original_total += file_total;
-                    scrubber.scrub(&mut db, &mut target).unwrap();
-                }
-                scrubber.update().unwrap();
-            }
-            (scrubber.delta_stored(), scrubber.fp_table_size(), scrubber.sf_table_size())
-        }
-        4 => {
-            let arr: [u32; 4] = tier_list.try_into().unwrap();
-            let tc = TierConfig::new(arr);
-            let sf_gen = PalantirHasher::new(7, tier_list.to_vec());
-            let mut scrubber = PalantirScrubber::new(
-                sf_gen, GdeltaEncoder, tc,
-                LifecycleManager::<4>::default_configs(),
-            );
-            for files in kernel_files {
-                for data in files {
-                    let (mut db, file_total) = chunk_file(data, chunk_size);
-                    if db.is_empty() { continue; }
-                    original_total += file_total;
-                    scrubber.scrub(&mut db, &mut target).unwrap();
-                }
-                scrubber.update().unwrap();
-            }
-            (scrubber.delta_stored(), scrubber.fp_table_size(), scrubber.sf_table_size())
-        }
-        5 => {
-            let arr: [u32; 5] = tier_list.try_into().unwrap();
-            let tc = TierConfig::new(arr);
-            let sf_gen = PalantirHasher::new(7, tier_list.to_vec());
-            let mut scrubber = PalantirScrubber::new(
-                sf_gen, GdeltaEncoder, tc,
-                LifecycleManager::<5>::default_configs(),
-            );
-            for files in kernel_files {
-                for data in files {
-                    let (mut db, file_total) = chunk_file(data, chunk_size);
-                    if db.is_empty() { continue; }
-                    original_total += file_total;
-                    scrubber.scrub(&mut db, &mut target).unwrap();
-                }
-                scrubber.update().unwrap();
-            }
-            (scrubber.delta_stored(), scrubber.fp_table_size(), scrubber.sf_table_size())
-        }
-        _ => unreachable!(),
-    };
 
+    let mut file_id = 0u64;
+    for files in kernel_files {
+        for data in files {
+            let chunker = FastChunker::new(chunk_size);
+            let mut handle = fs.create_file(format!("f{}", file_id), chunker).unwrap();
+            fs.write_to_file(&mut handle, data).unwrap();
+            fs.close_file(handle).unwrap();
+            file_id += 1;
+        }
+    }
+
+    let cdc_ratio = fs.cdc_dedup_ratio();
+    fs.scrub().unwrap();
+    let total_ratio = fs.total_dedup_ratio();
     let elapsed = start.elapsed();
-    let stored_total = target.total_bytes() as u64;
-    let chunks_total = target.len();
-    let dedup_ratio = if original_total > 0 {
-        original_total as f64 / stored_total as f64
-    } else {
-        1.0
-    };
-    let throughput_mbps = if elapsed.as_secs_f64() > 0.0 {
-        (original_total as f64 / elapsed.as_secs_f64()) / (1024.0 * 1024.0)
+
+    let stored_mb = (original_total as f64 / total_ratio) / (1024.0 * 1024.0);
+    let orig_mb = original_total as f64 / (1024.0 * 1024.0);
+    let mbps = if elapsed.as_secs_f64() > 0.0 {
+        original_total as f64 / elapsed.as_secs_f64() / (1024.0 * 1024.0)
     } else {
         0.0
     };
 
     println!(
-        "{:<20} chunks={:<7} dedup_ratio={:<10.6} stored_mb={:<10.3} orig_mb={:<10.3} delta_cnt={:<6} fp_size={:<8} sf_size={:<8} elapsed_s={:<10.2} throughput_mbps={:.2}",
-        name,
-        chunks_total,
-        dedup_ratio,
-        stored_total as f64 / (1024.0 * 1024.0),
-        original_total as f64 / (1024.0 * 1024.0),
-        delta_stored,
-        fp_size,
-        sf_size,
-        elapsed.as_secs_f64(),
-        throughput_mbps,
+        "{:<20} cdc={:<8.4} tot={:<8.4} stored_mb={:<10.3} orig_mb={:<10.3} elapsed_s={:<10.2} mbps={:.2}",
+        name, cdc_ratio, total_ratio, stored_mb, orig_mb, elapsed.as_secs_f64(), mbps,
     );
 }
 
@@ -211,7 +99,7 @@ fn main() {
     let kernel_dirs = [
         "/home/mak/RustroverProjects/marline/linux-3.4.5",
         "/home/mak/RustroverProjects/marline/linux-3.4.6",
-        // "/home/mak/RustroverProjects/marline/linux-3.4.7",
+        "/home/mak/RustroverProjects/marline/linux-3.4.7",
     ];
 
     let mut kernel_files: Vec<Vec<Vec<u8>>> = Vec::new();
@@ -223,12 +111,68 @@ fn main() {
     }
 
     println!(
-        "{:<20} {:<8} {:<13} {:<11} {:<11} {:<9} {:<9} {:<9} {:<12} {:<12}",
-        "config", "chunks", "dedup_ratio", "stored_mb", "orig_mb",
-        "delta_cnt", "fp_size", "sf_size", "elapsed_s", "mbps"
+        "{:<20} {:<9} {:<9} {:<11} {:<11} {:<12} {:<12}",
+        "config", "cdc_ratio", "total_dedup_ratio", "stored_mb", "orig_mb", "elapsed_s", "mbps"
     );
 
     for cfg in configs() {
-        run_metrics(cfg.name, &cfg.tier_list, cfg.features_num_override, &kernel_files);
+        let sf_gen = if let Some(fn_val) = cfg.features_num_override {
+            PalantirHasher::with_features_num(7, cfg.tier_list.clone(), fn_val)
+        } else {
+            PalantirHasher::new(7, cfg.tier_list.clone())
+        };
+
+        match cfg.tier_list.len() {
+            1 => {
+                let arr: [u32; 1] = cfg.tier_list.as_slice().try_into().unwrap();
+                let tier_config = if let Some(fn_val) = cfg.features_num_override {
+                    TierConfig::with_features_num(arr, fn_val)
+                } else {
+                    TierConfig::new(arr)
+                };
+                let scrubber = PalantirScrubber::new(
+                    sf_gen, GdeltaEncoder, tier_config,
+                    LifecycleManager::<1>::default_configs(),
+                );
+                run_metrics(cfg.name, scrubber, &kernel_files);
+            }
+            2 => {
+                let arr: [u32; 2] = cfg.tier_list.as_slice().try_into().unwrap();
+                let tier_config = TierConfig::new(arr);
+                let scrubber = PalantirScrubber::new(
+                    sf_gen, GdeltaEncoder, tier_config,
+                    LifecycleManager::<2>::default_configs(),
+                );
+                run_metrics(cfg.name, scrubber, &kernel_files);
+            }
+            3 => {
+                let arr: [u32; 3] = cfg.tier_list.as_slice().try_into().unwrap();
+                let tier_config = TierConfig::new(arr);
+                let scrubber = PalantirScrubber::new(
+                    sf_gen, GdeltaEncoder, tier_config,
+                    LifecycleManager::<3>::default_configs(),
+                );
+                run_metrics(cfg.name, scrubber, &kernel_files);
+            }
+            4 => {
+                let arr: [u32; 4] = cfg.tier_list.as_slice().try_into().unwrap();
+                let tier_config = TierConfig::new(arr);
+                let scrubber = PalantirScrubber::new(
+                    sf_gen, GdeltaEncoder, tier_config,
+                    LifecycleManager::<4>::default_configs(),
+                );
+                run_metrics(cfg.name, scrubber, &kernel_files);
+            }
+            5 => {
+                let arr: [u32; 5] = cfg.tier_list.as_slice().try_into().unwrap();
+                let tier_config = TierConfig::new(arr);
+                let scrubber = PalantirScrubber::new(
+                    sf_gen, GdeltaEncoder, tier_config,
+                    LifecycleManager::<5>::default_configs(),
+                );
+                run_metrics(cfg.name, scrubber, &kernel_files);
+            }
+            _ => unreachable!(),
+        }
     }
 }
